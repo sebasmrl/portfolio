04a912ca4b607df8f720f826d9e1ddb85846eb63
Sebastian Morales <davesebastian99@gmail.com>
2024-10-31 17:03:52 -0500

    Fix: Mobile layout ProjectSection base

--- src/components/sections/projects/ProjectsSection.tsx
+++ src/components/sections/projects/ProjectsSection.tsx
@@ -7,7 +7,7 @@ export const ProjectsSection = () => {
     <section id="projects" className='mt-20 py-4'>
       <h2 className='pb-8 text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-violet-600'>Proyectos</h2>
 
-        <BentoGrid className='dark grid grid-cols-3 gap-2'>
+        <BentoGrid className='dark grid grid-cols-1 sm:grid-cols-3 gap-y-2 sm:gap-x-2 '>
             <BentoCard 
                 name={'Nombre del proyecto - Card'} 
                 className={'text-xl border col-span-1 max-h-80'} 
