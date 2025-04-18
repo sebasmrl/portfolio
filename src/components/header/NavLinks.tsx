8832e93521a7681ff8527fcf074e6b17b6c5700d
Sebastian Morales <davesebastian99@gmail.com>
2025-04-17 22:26:31 -0500

    Modify: NavLinks classNames

--- src/components/header/NavLinks.tsx
+++ src/components/header/NavLinks.tsx
@@ -23,7 +23,7 @@ export const NavLinks = () => {
         <nav className="hidden lg:flex text-lg font-medium gap-4 min-h-full text-center self-stretch ">
             {
                 links.map( link=>(
-                    <Link key={link.url} href={link.url} className={'cursor-pointer content-center hover:shadow-[inset_0_-2px_0_rgb(226,232,240)] transition-all hover:text-white  dark:shadow-none'}>
+                    <Link key={link.url} href={link.url} className={'cursor-pointer content-center hover:shadow-[inset_0_-2px_0_rgb(226,232,240)] transition-all hover:text-white'}>
                         {link.title}
                     </Link>
                 ))
